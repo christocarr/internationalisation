9ca282f574236372980bd5b5789251b9ca8d806e
christocarr <christocarr@outlook.com>
2023-04-10 15:11:22 +0100

    Add internationalisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import i18next from "i18next";
+import { useTranslation } from "react-i18next";
+import "./App.css";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
+const languages = [
+  { code: "en", name: "English", country_code: "gb" },
+  { code: "af", name: "Afrikaans", country_code: "za" },
+  { code: "de", name: "Deutsch", country_code: "de" },
+  { code: "es", name: "Español", country_code: "es" },
+];
 
 function App() {
+  const [clicks, setClicks] = useState(0);
+  const [langMenuShown, setLangMenuShown] = useState(false);
+
+  const { t } = useTranslation();
+
+  const handleClick = () => {
+    setClicks(() => clicks + 1);
+  };
+
+  const handleLangMenuToggle = () => {
+    setLangMenuShown(!langMenuShown);
+  };
+
+  const handleLanguageSelection = (langCode: string) => {
+    i18next.changeLanguage(langCode);
+    setLangMenuShown(!langMenuShown);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <button className="language_menu_toggle" onClick={handleLangMenuToggle}>
+          {t("choose_a_language")}
+        </button>
+        <ul className={langMenuShown ? `show_lang_menu` : `hide_lang_menu`}>
+          {languages.map((lang) => (
+            <li key={lang.code}>
+              <button
+                className="language_selector"
+                onClick={() => handleLanguageSelection(lang.code)}
+              >
+                <span className={`fi fi-${lang.country_code}`}></span>
+                {lang.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="container">
+        <h2>{t("key")}</h2>
+        <p>{t("clicks", { clicks })}</p>
+        <button onClick={handleClick}>{t("click_me")}</button>
+      </div>
     </div>
   );
 }
-
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+
+    //lng: document.querySelector("html")?.lang, // if you're using a language detector, do not define the lng option]
+    debug: true,
+    fallbackLng: "en",
+    detection: {
+      order: [
+        "cookie",
+        "htmlTag",
+        "navigator",
+        "localStorage",
+        "path",
+        "subdomain",
+      ],
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}/translation.json",
+    },
+  });
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
